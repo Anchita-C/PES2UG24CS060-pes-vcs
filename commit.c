e16e9097f65f5defdce7267f2bbd71bb7103868c
Anchita-C <anchita.chhibba@gmail.com>
2026-04-16 18:00:51 +0000

    filling in todo of commit_create function

--- commit.c
+++ commit.c
@@ -194,8 +194,14 @@ int head_update(const ObjectID *new_commit) {
 //
 // Returns 0 on success, -1 on error.
 int commit_create(const char *message, ObjectID *commit_id_out) {
-    // TODO: Implement commit creation
-    // (See Lab Appendix for logical steps)
-    (void)message; (void)commit_id_out;
+    Commit c;
+
+    // Step 1: build tree from index (snapshot staged state)
+    if (tree_from_index(&c.tree) != 0) {
+        return -1;
+    }
+
+    (void)message;
+    (void)commit_id_out;
     return -1;
 }
